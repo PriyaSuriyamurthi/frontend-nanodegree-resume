771ea7392c799cdcfc47c2a0289c7a71e19fedeb
priya suriyamurthi <priya.suriyamurthi@gmail.com>
2016-06-09 09:41:46 -0500

    first commit

--- js/helper.js
+++ js/helper.js
@@ -15,7 +15,7 @@ replace the %data% placeholder text you see in them.
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">Contact</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
@@ -55,7 +55,7 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+var internationalizeButton = '<button id="intern">Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
@@ -110,9 +110,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -131,17 +131,17 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
+    educ.schools.forEach(function(school){
       locations.push(school.location);
     });
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    work.jobs.forEach(function(job){
+    work.works.forEach(function(job){
       locations.push(job.location);
     });
 
@@ -177,7 +177,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+       infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -239,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,160 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+    "name": "Priya Suriyamurthi",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile" :6824012998,
+        "email": "Priya.suriyamurthi@gmail.com" ,
+        "github": "https://github.com/PriyaSuriyamurthi/frontend-nanodegree-resume.git",
+        "location":"Austin"
+    },
+    "biopic": "C:\\Users\\Vinodh\\Documents\\Portfolio\\images\\Priya.png",
+    "welcomemessage"    :"Welcome all",
+    "skills": ["cool","energetic","enthusiasm"]
+};
+
+$("#header").prepend(HTMLheaderRole.replace("%data%",bio.role));
+$("#header").prepend(HTMLheaderName.replace("%data%",bio.name));
+
+$("#topContacts").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+$("#topContacts").append(HTMLemail.replace("%data%",bio.contacts.email));
+$("#topContacts").append(HTMLgithub.replace("%data%",bio.contacts.github));
+$("#topContacts").append(HTMLlocation.replace("%data%",bio.contacts.location));
+
+$("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomemessage));
+$("#header").append(HTMLbioPic.replace("%data%",bio.biopic));
+
+if(bio.skills.length > 0 ){
+$("#header").append(HTMLskillsStart);
+for(i in bio.skills)
+{
+$("#skills").append(HTMLskills.replace("%data%",bio.skills[i]));
+}
+}
+
+var work = {
+    "works" : [{
+    "employer" : "CM First Group",
+    "title" : "Application Consultant",
+    "dates" : "Mar 2016 - Till date",
+    "location" : "Austin",
+    "description": "Analysis, Coding, Testing, Maintanence"},
+    {
+    "employer" : "CVS Caremark",
+    "title" : "Technology Lead",
+    "dates" : "Jan 2015 - Jan 2016",
+    "location" : "Richardson",
+    "description": "Analysis, Coding, Testing, Maintanence"},
+    {
+    "employer" : "Infosys Limited",
+    "title" : "Technology Analyst",
+    "dates" : "Sep 2008 - Jan 2016",
+    "location" : "Fort Worth",
+    "description": "Analysis, Coding, Testing, Maintanence"
+    }
+]}
+
+var educ ={
+    "schools" : [{
+    "name" : "Savithri Vidyasala",
+    "location": "Trichy",
+    "degree" : "Higher Secondary",
+    "major" : "Biology-Physics-Chemistry-Maths",
+    "dates" : "Jun 2002 - Mar 2004"},
+    {
+    "name" : "Anna University",
+    "location": "Trichy",
+    "degree" : "Bachelor of Engineering",
+    "major" : "electronics of communication",
+    "dates" : "Sep 2004 - May 2008"
+    }
+    ],
+    "onlineCourses":[{
+        "title":"Front End Developer",
+        "school":"Udacity",
+        "dates":"May 2016",
+        "url":"www.udacity.com"
+    }]
+}
+
+for(i in educ.schools)
+{
+$("#education").append(HTMLschoolStart);
+$(".education-entry:last").append(HTMLschoolName.replace("%data%",educ.schools[i].name));
+$(".education-entry:last").append(HTMLschoolLocation.replace("%data%",educ.schools[i].location));
+$(".education-entry:last").append(HTMLschoolDegree.replace("%data%",educ.schools[i].degree));
+$(".education-entry:last").append(HTMLschoolMajor.replace("%data%",educ.schools[i].major));
+$(".education-entry:last").append(HTMLschoolDates.replace("%data%",educ.schools[i].dates));
+}
+
+for(i in educ.onlineCourses)
+{
+$(".education-entry:last").append(HTMLonlineClasses);
+$(".education-entry:last").append(HTMLonlineTitle.replace("%data%",educ.onlineCourses[i].title));
+$(".education-entry:last").append(HTMLonlineSchool.replace("%data%",educ.onlineCourses[i].school));
+$(".education-entry:last").append(HTMLonlineDates.replace("%data%",educ.onlineCourses[i].dates));
+$(".education-entry:last").append(HTMLonlineURL.replace("%data%",educ.onlineCourses[i].url));
+}
+
+displayWork();
+function displayWork() {
+for (i in work.works)
+{
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry:last").append(HTMLworkEmployer.replace("%data%",work.works[i].employer));
+$(".work-entry:last").append(HTMLworkTitle.replace("%data%",work.works[i].title));
+$(".work-entry:last").append(HTMLworkLocation.replace("%data%",work.works[i].location));
+$(".work-entry:last").append(HTMLworkDates.replace("%data%",work.works[i].dates));
+$(".work-entry:last").append(HTMLworkDescription.replace("%data%",work.works[i].description));
+}
+}
+
+$(document).click(function(loc)
+    {
+        var x=loc.pageX,y=loc.pageY;
+        logClicks(x,y);
+    });
+
+$('#main').append(internationalizeButton);
+
+
+function inName(name_main){
+    var name_split = name_main.trim().split(" ");
+    name_main = name_split[0].charAt(0).toUpperCase() + name_split[0].slice(1).toLowerCase() + " " + name_split[1].toUpperCase();
+    return name_main;
+}
+
+$('#intern').click(function(){
+    bio.name = inName(bio.name);
+    $("#header").prepend(HTMLheaderName.replace("%data%",bio.name));
+});
+
+var projects ={
+    "projects":[{
+    "title":"Portfolio",
+    "dates": "Apr 2016",
+    "description": "Create a portfolio using HTML and CSS",
+    "images":"images/portfolio.png"
+    }]
+}
+
+projects.display = function() {
+for (i in projects.projects)
+{
+$("#projects").append(HTMLprojectStart);
+$(".project-entry:last").append(HTMLprojectTitle.replace("%data%",projects.projects[i].title));
+$(".project-entry:last").append(HTMLprojectDates.replace("%data%",projects.projects[i].dates));
+$(".project-entry:last").append(HTMLprojectDescription.replace("%data%",projects.projects[i].description));
+$(".project-entry:last").append(HTMLprojectImage.replace("%data%",projects.projects[i].images));
+}
+}
+
+projects.display();
+
+$("#mapDiv").append(googleMap);
+
+
+
